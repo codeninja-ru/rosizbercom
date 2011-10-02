a239f7a4ee78686cb6671ba0c95c2cd18e2b4d19
codeninja <abuse_postmaster@mail.ru>
2011-10-02 13:15:36 +1100

    asynchronous loading of vk/google/twitter buttons

--- duma/public_html/js/script.js
+++ duma/public_html/js/script.js
@@ -1,10 +1,32 @@
-/* Author:
+/* Author: codeninja_ru Mayorov Vitaliy
 
 */
 
+yepnope([{
+	load: "http://userapi.com/js/api/openapi.js?34",		
+	complete: function() {
+	    VK.init({apiId: 2628061, onlyWidgets: true});
+		widget_vk_init();
+	}
+},
+]);
 
+yepnope(['http://platform.twitter.com/widgets.js', 'https://apis.google.com/js/plusone.js']);
 
-
-
-
-
+function widget_vk_init() {
+	VK.Widgets.Like("vk_like_kprf", {type: "mini", text: "Я голосую", pageTitle: "Я голосую за партию &laquo;КПРФ&raquo;", pageDescription: "Прими участие в правдивом интернет-голосовании."}, 1001);
+	
+	VK.Widgets.Like("vk_like_ldpr", {type: "mini", text: "Я голосую", pageTitle: "Я голосую за партию ЛДПР", pageDescription: "Прими участие в правдивом интернет-голосовании."}, 1002);
+	
+	VK.Widgets.Like("vk_like_er", {type: "mini", text: "Я голосую", pageTitle: "Я голосую за партию &laquo;Единая Россия&raquo;", pageDescription: "Прими участие в правдивом интернет-голосовании."}, 1003);
+	
+	VK.Widgets.Like("vk_like_sr", {type: "mini", text: "Я голосую", pageTitle: "Я голосую за партию &laquo;Справедливая Россия&raquo;", pageDescription: "Прими участие в правдивом интернет-голосовании."}, 1004);
+	
+	VK.Widgets.Like("vk_like_yabloko", {type: "mini", text: "Я голосую", pageTitle: "Я голосую за партию &laquo;ЯБЛОКО&raquo;", pageDescription: "Прими участие в правдивом интернет-голосовании."}, 1003);
+	
+	VK.Widgets.Like("vk_like_pr", {type: "mini", text: "Я голосую", pageTitle: "Я голосую за партию &laquo;Патриоты России&raquo;", pageDescription: "Прими участие в правдивом интернет-голосовании."}, 1006);
+	
+	VK.Widgets.Like("vk_like_pd", {type: "mini", text: "Я голосую", pageTitle: "Я голосую за партию &laquo;Правое дело&raquo;", pageDescription: "Прими участие в правдивом интернет-голосовании."}, 1007);
+	
+	VK.Widgets.Comments("vk_comments", {limit: 10, width: "460", attach: false});
+}
